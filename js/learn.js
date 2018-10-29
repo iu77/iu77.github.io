447c431fdc062c6474563bf9081294d0f1546b56
iu77 <42781453+iu77@users.noreply.github.com>
2018-10-29 20:49:50 +0200

    jssalutnick

--- js/learn.js
+++ js/learn.js
@@ -1,4 +1,7 @@
 console.info(18);
+function sayWellcome() {
+  console.info("Salut " + prenume + ",bine ai venit la noi");
+}
 var motto, favorite, links;
 motto = document.getElementById("motto");
 console.log(motto);
@@ -11,8 +14,15 @@ var age = 41;
 var msg1 = 'ai zis "Nick"?';
 var msg2 = 'Nu ,am zis "Nicolae"';
 var template = 'text cu ghilimele"sau simple';
+function sayWellcome() {
+  console.info("Salut Ionel,bine ai venit la noi");
+}
 var skills = ["html", "css", "js"];
 prenume = "Nick";
 console.info("toate variabilele au fost initializate");
 console.log("nume");
-console.debug("cum te cheama?-" + prenume);
+console.debug("auzi...? dar cum te cheama?-" + prenume);
+sayWellcome();
+sayWellcome();
+
+// schimb valutar//
