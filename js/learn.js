515b7fb38a8a069b4d3f0b4fcf219be02c72a68d
iu77 <42781453+iu77@users.noreply.github.com>
2018-10-29 21:37:26 +0200

    jsextraxtbanc2910

--- js/learn.js
+++ js/learn.js
@@ -26,3 +26,30 @@ sayWellcome();
 sayWellcome();
 
 // schimb valutar//
+var cursEUR = 4.5;
+var myEur = 20;
+var myRON = 0;
+var friendEUR = 100;
+var friendRON = 0;
+//opreatori sunt: *, /,-
+
+myRON = myEur * cursEUR;
+console.info(" eu am " + myRON + " RON ");
+friendRON = friendEUR * cursEUR * 0.99;
+console.info(" tu ai " + friendRON + " RON ");
+friendRON = friendEUR * cursEUR - (friendEUR * cursEUR * 1) / 100;
+console.info(" tu ai " + friendRON + " RON ");
+
+function extractFromATM(ammount) {
+  console.info("==== ==== ==== ==== ====");
+  console.info("suma extrasa este: " + ammount);
+  var comision = ammount * 0.01;
+  if (comision < 2.5) {
+    console.warn("comision minim aplicat");
+    comision = 2.5;
+  }
+  console.info("comision aplicat:" + comision);
+  console.info("==== ==== ==== ==== ====");
+}
+extractFromATM(1000);
+extractFromATM(100);
