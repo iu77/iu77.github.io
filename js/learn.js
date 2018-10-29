24114007a15324badc804078104d688bb378c58b
iu77 <42781453+iu77@users.noreply.github.com>
2018-10-29 19:57:00 +0200

    cv js

---
+++ js/learn.js
@@ -0,0 +1,9 @@
+console.info(18);
+var motto, favorite, links;
+motto = document.getElementById("motto");
+console.log(motto);
+
+var nume = "Ungur";
+var prenume = "Ionel";
+var owner = "Ungur 'I";
+var msg1;
