fd2f3f0c5dd042597c940e74b0934fe4994d0a7e
iu77 <42781453+iu77@users.noreply.github.com>
2018-10-29 20:26:51 +0200

    adstringvarless

---
+++ js/learn.js
@@ -0,0 +1,18 @@
+console.info(18);
+var motto, favorite, links;
+motto = document.getElementById("motto");
+console.log(motto);
+
+var nume = "Ungur";
+var prenume = "Ionel";
+var owner = "Ungur 'I";
+var employed = true;
+var age = 41;
+var msg1 = 'ai zis "Nick"?';
+var msg2 = 'Nu ,am zis "Nicolae"';
+var template = 'text cu ghilimele"sau simple';
+var skills = ["html", "css", "js"];
+prenume = "Nick";
+console.info("toate variabilele au fost initializate");
+console.log("nume");
+console.debug("cum te cheama?-" + prenume);
