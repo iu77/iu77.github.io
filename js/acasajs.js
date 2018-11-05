f9c32b22947d702f4428ece954e0bdb138963af2
iu77 <42781453+iu77@users.noreply.github.com>
2018-11-05 19:55:25 +0200

    add global vars ATM funds

--- js/acasajs.js
+++ js/acasajs.js
@@ -1,4 +1,4 @@
-console.info(1);
+console.info(18);
 function sayWellcome() {
   console.info("Salut " + prenume + ",bine ai venit la noi");
 }
@@ -10,7 +10,7 @@ var nume = "Ungur";
 var prenume = "Ionel";
 var owner = "Ungur 'I";
 var employed = true;
-var age = 40;
+var age = 41;
 var msg1 = 'ai zis "Nick"?';
 var msg2 = 'Nu ,am zis "Nicolae"';
 var template = 'text cu ghilimele"sau simple';
@@ -24,13 +24,15 @@ console.log("nume");
 console.debug("auzi...? dar cum te cheama?-" + prenume);
 sayWellcome();
 sayWellcome();
+
 // schimb valutar//
 var cursEUR = 4.5;
-var myEur = 100;
+var myEur = 20;
 var myRON = 0;
 var friendEUR = 100;
 var friendRON = 0;
 //opreatori sunt: *, /,-
+
 myRON = myEur * cursEUR;
 console.info(" eu am " + myRON + " RON ");
 friendRON = friendEUR * cursEUR * 0.99;
@@ -38,38 +40,16 @@ console.info(" tu ai " + friendRON + " RON ");
 friendRON = friendEUR * cursEUR - (friendEUR * cursEUR * 1) / 100;
 console.info(" tu ai " + friendRON + " RON ");
 
-var ATMFunds = 50000;
-var funds = 5000;
-
-function getExtractFee(ammount) {
+function extractFromATM(ammount) {
+  console.info("==== ==== ==== ==== ====");
+  console.info("suma extrasa este: " + ammount);
   var comision = ammount * 0.01;
   if (comision < 2.5) {
     console.warn("comision minim aplicat");
     comision = 2.5;
   }
-  return comision;
-}
-
-function extractFromATM(ammount) {
-  console.info("==== ==== ==== ==== ====");
-  console.info("suma extrasa este: " + ammount);
-  var comision = getExtractFee();
-  var totalExtract = ammount + comision;
-  if (totalExtract > funds) {
-    console.error("insuficient funds");
-    return;
-  }
-  if (ATMFunds < totalExtract) {
-    console.error(" ATM insuficient founds");
-  }
-
-  funds = funds - totalExtract;
-  console.info("Sold curent: " + funds);
   console.info("comision aplicat:" + comision);
   console.info("==== ==== ==== ==== ====");
 }
-extractFromATM(1005); //nu e multiplu de 10
-extractFromATM(100000); //solduri insuficiente
-extractFromATM(3000);
-extractFromATM(3000);
-extractFromATM(3000);
+extractFromATM(1000);
+extractFromATM(100);
