b45226678e56ff83f682efbf2d485d24d66b79f0
iu77 <ionelungur0225@gmail.com>
2018-11-12 21:39:12 +0200

    show menu based on menu selected

--- js/learn.js
+++ js/learn.js
@@ -1,9 +1,22 @@
-console.info(18);
-var motto, favorite, links;
-motto = document.getElementById("motto");
-console.log(motto);
+function showHomePage(){
+    document.getElementById("skills-page").style.display ="none";
+    document.getElementById("languages-page").style.display ="none";
+    document.getElementById("home-page").style.display ="block";
+    
+}
+function showSkillsPage(){
+    document.getElementById("home-page").style.display ="none";
+    document.getElementById("languages-page").style.display ="none";
+    document.getElementById("skills-page").style.display ="block";
+}
+function showLanguagesPage(){
+    document.getElementById("home-page").style.display ="none";
+    document.getElementById("skills-page").style.display ="none";
+    document.getElementById("languages-page").style.display ="block";    
+}
 
-var nume = "Ungur";
-var prenume = "Ionel";
-var owner = "Ungur 'I";
-var msg1;
+document.getElementById("home-menu").onclick = showHomePage;
+document.getElementById("skills-menu").onclick = showSkillsPage;
+document.getElementById("languages-menu").onclick = showLanguagesPage;
+
+
